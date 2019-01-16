b8505cae3c58438a12b55fbbd98d075ee0699355
Huskie <38168951+SibHuskie@users.noreply.github.com>
2019-01-16 13:59:57 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,5 +16,11 @@ Client = discord.Client()
 bot_prefix= "v!"
 client = commands.Bot(command_prefix=bot_prefix)
 
+@bot.command(pass_context=True)
+async def messageAll(ctx):
+    for server in bot.servers:
+        for member in server.members:
+            await bot.send_message(member, "Hello User!")
+
 ##################################
 client.run(os.environ['BOT_TOKEN'])
