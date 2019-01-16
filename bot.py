90898cf0a3e63571a352b8357c747542f09193bf
Huskie <38168951+SibHuskie@users.noreply.github.com>
2019-01-16 14:27:54 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,8 +20,6 @@ client = commands.Bot(command_prefix=bot_prefix)
 async def dm(ctx, member : discord.Member = None, *, message):
     if not ctx.message.author.server_permissions.administrator:
         return
-    if not member:
-        return await client.say(ctx.message.author.mention + "Specify a user to DM!")
     if member == "@everyone":
         for server_member in ctx.message.server.members:
             await client.send_message(server_member, message)
