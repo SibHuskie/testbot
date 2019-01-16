f3aa46cb350379a3f0b2087ac24d47d0db7c4304
Huskie <38168951+SibHuskie@users.noreply.github.com>
2019-01-16 14:14:34 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,11 +16,15 @@ Client = discord.Client()
 bot_prefix= "v!"
 client = commands.Bot(command_prefix=bot_prefix)
 
-@client.command(pass_context=True)
-async def messageAll(ctx):
-    for server in client.servers:
-        for member in server.members:
-            await client.send_message(member, "Hello User!")
+@client.command(pass_context = True)
+async def dm(ctx, member : discord.Member = None, *, message):
+    if not ctx.message.author.server_permissions.administrator:
+        return
+    if not member:
+        return await client.say(ctx.message.author.mention + "Specify a user to DM!")
+    if member == "@everyone":
+        for server_member in ctx.message.server.members:
+            await client.send_message(server_member, message)
 
 ##################################
 client.run(os.environ['BOT_TOKEN'])
